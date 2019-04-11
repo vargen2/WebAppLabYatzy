2083568f2c018681044f2bc3fe3665230faa49c6
vargen2 <hjaarn2@gmail.com>
2019-04-11 15:40:15 +0200

    basic functionallity

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -4,53 +4,116 @@
     <p>Game Round: {{gameRound}}</p>
     <p>Roll Round: {{rollRound}}</p>
     <div class="dicerow">
-        <div class="dice"  v-for="(dice, index) in dices" 
-                v-bind:key="index"
-                v-on:click="lockDice(index)">
-                {{dice.side}}
-                <span v-if="dice.locked">locked</span>
+      <div
+        class="dice"
+        v-for="(dice, index) in dices"
+        v-bind:key="index"
+        v-on:click="lockDice(index)"
+        v-bind:class="{ avoidclicks: isNotLockable }"
+      >
+        <div v-if="rollRound != 0">
+          {{dice.side}}
+          <span v-if="dice.locked">locked</span>
         </div>
+      </div>
     </div>
     <button @click="roll">Roll</button>
+    <div class="scoresheet">
+      <div class="scoresection">
+        <div
+          class="scoringbox"
+          v-for="(score, index) in upperScores"
+          v-bind:key="index"
+          v-on:click="selectUpperScore(index)"
+          v-bind:class="{avoidclicks: score.dices.length>0}"
+        >
+          {{score.name}}
+          {{score.dices}}
+        </div>
+      </div>
+      <div class="scoresection">
+        <div
+          class="scoringbox"
+          v-for="(score, index) in upperScores"
+          v-bind:key="index"
+          v-on:click="selectUpperScore(index)"
+          v-bind:class="{avoidclicks: score.dices.length>0}"
+        >
+          {{score.name}}
+          {{score.dices}}
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-
 export default {
   name: "Game",
   computed: {
-    dices: function() {        
-      return this.$store.state.dices
+    dices: function() {
+      return this.$store.state.dices;
+    },
+    isNotLockable() {
+      return this.$store.state.rollRound === 0;
     },
     gameRound: function() {
-        return this.$store.state.gameRound
+      return this.$store.state.gameRound;
     },
     rollRound: function() {
-        return this.$store.state.rollRound
+      return this.$store.state.rollRound;
+    },
+    upperScores: function() {
+      return this.$store.state.upperScores;
     }
   },
-  methods:{
-      roll: function () {
-        this.$store.dispatch('roll')
-      },
-      lockDice: function (index) {
-          this.$store.dispatch('lockDice',index)
-      }
+  methods: {
+    roll: function() {
+      this.$store.dispatch("roll");
+    },
+    lockDice: function(index) {
+      this.$store.dispatch("lockDice", index);
+    },
+    selectUpperScore: function(index) {
+      this.$store.dispatch("selectUpperScore", index);
+    }
   }
-}
+};
 </script>
 
 <style scoped>
 .dicerow {
-    display: flex;
-    justify-content: center;
+  display: flex;
+  justify-content: center;
 }
 
 .dice {
-    margin: 2em;
-    height: 3em;
-    width: 3em;
-    background-color: springgreen
+  margin: 2em;
+  height: 3em;
+  width: 3em;
+  background-color: springgreen;
+  cursor: pointer;
+}
+
+.avoidclicks {
+  pointer-events: none;
+}
+
+.scoresheet {
+  display: flex;  
+  justify-content: space-evenly;
+}
+
+.scoresection {
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+}
+
+.scoringbox {
+  border: 1px solid #000;
+  width: 10em;
+  height: 4em;
+  cursor: pointer;
 }
 </style>
--- src/store.js
+++ src/store.js
@@ -6,30 +6,49 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     dices: [],
+    upperScores: [],
     gameRound: Number,
     rollRound: Number
   },
   mutations: {
     initGame (state) {
       state.dices=[{side:1,locked:false},{side:1,locked:false},{side:1,locked:false},{side:1,locked:false},{side:1,locked:false}]
+      state.upperScores=[
+        {name:'Aces',dices:[]},
+        {name:'Twos',dices:[]},
+        {name:'Threes',dices:[]}
+
+      ]
       state.gameRound=0
       state.rollRound=0
     },
     roll (state) {
+      if (state.rollRound >=3) {
+        return
+      }
       for (let i = 0; i < state.dices.length; i++) {
         if (!state.dices[i].locked) {
           state.dices[i].side=Math.floor(Math.random()*6+1) 
         }        
       }
-      
       state.rollRound++
-      if (state.rollRound >=3) {
-        state.rollRound=0
-        state.gameRound++
-      }
     },
     lockDice (state, index) {
+      if (state.rollRound===0) {
+        return
+      }
       state.dices[index].locked = !state.dices[index].locked 
+    },
+    selectUpperScore (state, index) {
+      if (state.rollRound ==0 || state.upperScores[index].dices.length>0) {
+         return
+      }  
+      state.rollRound=0
+      state.gameRound++
+      state.upperScores[index].dices=state.dices.map(d=>d.side)      
+      for (const dice of state.dices) {
+        dice.locked=false
+      }
     }
   },
   actions: {
@@ -38,6 +57,9 @@ export default new Vuex.Store({
     },
     lockDice (state, index) {
       this.commit('lockDice',index)
+    },
+    selectUpperScore (state, index) {
+      this.commit('selectUpperScore',index)
     }
   }
 })
